403cae05950aa0f69d11c8bb4bb443e6bad6046d
soumy <soumysahu2006@gmail.com>
2026-08-24 20:56:36 +0530

    Add CPP lab

---
+++ CPP lab/ex 2 p1.cpp
@@ -0,0 +1,21 @@
+// sum of individual digits
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number"<<endl;
+    cin>>num;
+
+    int sum=0;
+    while(num!=0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    cout<<"Sum is "<<sum;
+
+    return 0;
+}
---
+++ CPP lab/ex 2 p2.cpp
@@ -0,0 +1,8 @@
+// first n term of a sequence
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    return 0;
+}
